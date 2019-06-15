921e3f1d062d56f00c4df17d7b003f819a41cefb
OlStoppa <olthespud@hotmail.com>
2019-06-15 12:23:41 +0800

    refactor

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,93 +1,112 @@
 import React from 'react';
-import DefaultInput from '../components/UI/DefaultInput';
-import MainText from '../components/UI/MainText';
 import { View, StyleSheet, Button, ImageBackground, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import uuid from 'uuid/v4';
+import DefaultInput from '../components/UI/DefaultInput';
+import MainText from '../components/UI/MainText';
 import { setInit } from '../store/actions/answers';
+import london from '../assets/london.jpg';
 
 class AuthScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Login'
-    };
-    state = {
-        value: ""
-    }
-    textChangeHandler = val => {
-        this.setState({
-            value: val
-        }, () => {console.log(this.state)});
-    }
-    handleSubmit = () => {
-        const token = {
-            name: this.state.value,
-            id: 1,
-            answers: {
-                part1: new Array(4).fill(new Array(8).fill(0)),
-                part2: new Array(4).fill(new Array(1).fill(0)),
-                part3: new Array(4).fill(new Array(3).fill(0))
-            }
-            };
-        this.props.onSetInit(token.answers, token.name, token.id);
-        const data = JSON.stringify(token);
-        this._storeData(data);
-
-        this.props.navigation.navigate('Dashboard', {name: this.state.value});
-    }
-    _storeData = async (data) => {
-        try {
-            await AsyncStorage.setItem('userToken', data);
-        } catch (error) {
-            console.log('error');
-        }
-        
-    }
-
-    render() {
-        return (
-            <ImageBackground 
-                source={{uri: "https://4.bp.blogspot.com/-gJmzke97eaI/WJRO6W2Z1RI/AAAAAAAAAKQ/hRxv5aOJzxoIPRdOeCTQg3ljFh8ykBc4QCLcB/s1600/ielts-examination.jpg"}}
-                style={{
-                    width: "100%",
-                    flex: 1
-            }}
-                >
-            <View style={styles.container}>
-                <View style={styles.form}>
-                <MainText>Choose a Username</MainText>
-                <DefaultInput 
-                    placeholder="username"
-                    onChangeText={this.textChangeHandler}
-                    />
-                <Button title="Login" onPress={this.handleSubmit}/>
-                </View>
-            </View>
-            </ImageBackground>
-        );
-    }
-}
-const mapDispatchToProps = dispatch => {
-    return {
-      onSetInit: (answers, name, id) => dispatch(setInit(answers, name, id))
-    };
+  static navigationOptions = {
+    title: 'Login',
   };
 
-export default connect(null, mapDispatchToProps)(AuthScreen);
+  state = {
+    value: '',
+  };
+
+  textChangeHandler = val => {
+    this.setState(
+      {
+        value: val,
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  handleSubmit = () => {
+    const token = {
+      name: this.state.value,
+      id: uuid(),
+      answers: {
+        part1: new Array(4).fill(new Array(8).fill(0)),
+        part2: new Array(4).fill(new Array(1).fill(0)),
+        part3: new Array(4).fill(new Array(3).fill(0)),
+      },
+    };
+    this.props.onSetInit(token.answers, token.name, token.id);
+    const data = JSON.stringify(token);
+    this._storeData(data);
+
+    this.props.navigation.navigate('Dashboard', { name: this.state.value });
+  };
+
+  _storeData = async data => {
+    try {
+      await AsyncStorage.setItem('userToken', data);
+    } catch (error) {
+      console.log('error');
+    }
+  };
+
+  render() {
+    return (
+      <ImageBackground
+        source={london}
+        style={{
+          width: '100%',
+          flex: 1,
+          position: 'absolute',
+          height: '100%',
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.form}>
+            <MainText>Choose a Username</MainText>
+            <DefaultInput placeholder="username" onChangeText={this.textChangeHandler} />
+            <Button title="Login" onPress={this.handleSubmit} disabled={!this.state.value} />
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    onSetInit: (answers, name, id) => dispatch(setInit(answers, name, id)),
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AuthScreen);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        
-    },
-    form: {
-        height: "50%",
-        justifyContent: "space-evenly",
-        width: "80%",
-        alignItems: "center",
-        backgroundColor: "#fff",
-        borderRadius: 20,
-        padding: 10,
-        opacity: .9
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    height: '50%',
+    justifyContent: 'space-evenly',
+    width: '80%',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 10,
+    opacity: 0.9,
+  },
 });
+
+AuthScreen.propTypes = {
+  onSetInit: PropTypes.func.isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- src/screens/SectionTwoReady.js
+++ src/screens/SectionTwoReady.js
@@ -4,34 +4,40 @@ import MainText from '../components/UI/MainText';
 import Header from '../components/UI/Header';
 
 class SectionTwoReady extends React.Component {
-    
+  render() {
+    const {testNumber} = this.props.navigation.state.params;
+    return (
+      <View>
+        <View style={{ width: '100%', alignSelf: 'flex-start' }}>
+          <Header>Part 2</Header>
+        </View>
+        <View style={styles.container}>
+          <MainText style={{ textAlign: 'center', fontSize: 20 }}>
+            You will have 60 seconds to plan your answer to the question card.
+          </MainText>
+          <MainText style={{ textAlign: 'center', fontSize: 20, marginBottom: 20 }}>
+            You may make notes.
+          </MainText>
 
-    render() {
-        const testNumber = this.props.navigation.state.params.testNumber;
-        return (
-            <View>
-                <View style={{ width: "100%", alignSelf: "flex-start" }}>
-                    <Header>Part 2</Header>
-                </View>
-                <View style={styles.container}>
-
-                    <MainText style={{textAlign: "center", fontSize: 20}}>You will have 60 seconds to plan your answer to the question card.</MainText>
-                    <MainText style={{textAlign: "center", fontSize: 20, marginBottom: 20}}>You may make notes.</MainText>
-
-                    <Button title="Ready" onPress={() => { this.props.navigation.navigate('PartTwo', { testNumber, part: 2 }) }} />
-                </View>
-            </View>
-        );
-    }
-};
+          <Button
+            title="Ready"
+            onPress={() => {
+              this.props.navigation.navigate('PartTwo', { testNumber, part: 2 });
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
 export default SectionTwoReady;
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 20
-    }
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
 });
