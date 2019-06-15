d6ff5b5c5c214bd767adbd2b5529d6ccc42579d2
OlStoppa <olthespud@hotmail.com>
2019-06-15 14:01:16 +0800

    refactor and flashing buttons

--- src/components/UI/StopButton.js
+++ src/components/UI/StopButton.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const StopButton = props => (
-  // <View style={styles.buttonContainer}>
-  <TouchableOpacity style={styles.recordButton} onPress={props.onStopRecord}>
-    <View style={styles.recordButton}>
-      <Icon name="md-square" size={80} color="red" />
-    </View>
-  </TouchableOpacity>
-  // </View>
-);
+class StopButton extends React.Component {
+  state = {
+    flash: true,
+  };
+
+  componentDidMount() {
+    this._flashInterval = setInterval(() => {
+      this.toggleFlash();
+    }, 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._flashInterval);
+  }
+
+  toggleFlash = () => {
+    this.setState(state => {
+      return {
+        flash: !state.flash,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <TouchableOpacity style={styles.recordButton} onPress={this.props.onStopRecord}>
+        <View style={styles.recordButton}>
+          <Icon name="md-square" size={60} color={this.state.flash ? 'red' : 'gray'} />
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 export default StopButton;
 
@@ -25,12 +49,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 5,
     borderColor: 'red',
-  },
-  buttonContainer: {
-    flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#c4dfe6',
   },
 });
--- src/store/reducers/root.js
+++ src/store/reducers/root.js
@@ -1,74 +1,63 @@
 import { ADD_ANSWER, DELETE_ANSWER, SET_INIT } from '../actions/actionTypes';
 
 const initialState = {
-    name: "",
-    id: 0,
-    answers: {
-        part1: new Array(4).fill(new Array(8).fill(0)),
-        part2: new Array(4).fill(new Array(1).fill(0)),
-        part3: new Array(4).fill(new Array(3).fill(0))
-    }
+  name: '',
+  id: '0',
+  answers: {
+    part1: new Array(4).fill(new Array(8).fill(0)),
+    part2: new Array(4).fill(new Array(1).fill(0)),
+    part3: new Array(4).fill(new Array(3).fill(0)),
+  },
 };
-
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
+  switch (action.type) {
+    case SET_INIT:
+      return {
+        answers: action.answers,
+        name: action.name,
+        id: action.id,
+      };
+    case ADD_ANSWER:
+      return {
+        ...state,
+        answers: {
+          ...state.answers,
 
-        case SET_INIT:
-            return {
-
-                answers: action.answers,
-                name: action.name,
-                id: action.id
-            };
-        case ADD_ANSWER:
-            return {
-                ...state,
-                answers: {
-                    ...state.answers,
-
-                    [action.part]: state.answers[action.part].map((arr, index) => {
-                        if (index === action.testNumber) {
-                            return arr.map((answer, index) => {
-                                if (index === action.index) {
-                                    return action.answer;
-                                }
-                                return answer;
-                            })
-                        }
-                        return arr;
-                    })
-
-
+          [action.part]: state.answers[action.part].map((arr, index) => {
+            if (index === action.testNumber) {
+              return arr.map((answer, index) => {
+                if (index === action.index) {
+                  return action.answer;
                 }
-            };
-        case DELETE_ANSWER:
-            return {
-                ...state,
-                answers: {
-                    ...state.answers,
-                    [action.part]: state.answers[action.part].map((arr, index) => {
-                        if (index === action.testNumber) {
-                            return arr.map((answer, index) => {
-                                if (index === action.index) {
-                                    return 0;
-                                }
-                                return answer;
-                            })
-                        }
-                        return arr;
-                    })
-
-
+                return answer;
+              });
+            }
+            return arr;
+          }),
+        },
+      };
+    case DELETE_ANSWER:
+      return {
+        ...state,
+        answers: {
+          ...state.answers,
+          [action.part]: state.answers[action.part].map((arr, index) => {
+            if (index === action.testNumber) {
+              return arr.map((answer, index) => {
+                if (index === action.index) {
+                  return 0;
                 }
-
-
-            };
-        default:
-            return state;
-    }
+                return answer;
+              });
+            }
+            return arr;
+          }),
+        },
+      };
+    default:
+      return state;
+  }
 };
-
-
 
 export default reducer;
